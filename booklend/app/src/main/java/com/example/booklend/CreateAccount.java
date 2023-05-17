12d17aed2c6ff9f52366aa83722d35f380c5f3c0
SuJin353 <131243461+SuJin353@users.noreply.github.com>
2023-05-17 11:44:42 +0700

    code login and create account done

--- booklend/app/src/main/java/com/example/booklend/CreateAccount.java
+++ booklend/app/src/main/java/com/example/booklend/CreateAccount.java
@@ -1,5 +1,6 @@
 package com.example.booklend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,41 +8,72 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccount extends AppCompatActivity {
-    private EditText et_username, et_email, et_phone_number, et_password, et_confirm_password;
-    private Button bt_back, bt_create;
+    private FirebaseAuth auth;
+    private EditText et_username, et_email, et_password, et_confirm_password;
+    private Button bt_create;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         Mapping();
-    }
-    public void onClick(View view){
-        switch(view.getId()){
-            case R.id.bt_back:
-            {
-                Intent intent = new Intent(CreateAccount.this, Login.class);
-                startActivity(intent);
-                break;
+
+        bt_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = et_username.getText().toString().trim();
+                String email = et_email.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+                String confirm_password = et_confirm_password.getText().toString().trim();
+
+                if (username.isEmpty())
+                {
+                    et_username.setError("User name can't be empty");
+                }
+                if (email.isEmpty())
+                {
+                    et_email.setError("Email can't be empty");
+                }
+                if (password.isEmpty())
+                {
+                    et_password.setError("Password can't be empty");
+                }
+                if (confirm_password.isEmpty() || confirm_password != password)
+                {
+                    et_username.setError("Please input same password");
+                }
+                else {
+                    auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(CreateAccount.this,"Sign up successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(CreateAccount.this, Login.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(CreateAccount.this,"Sign up failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
             }
-            case R.id.bt_create:
-            {
-                Intent intent = new Intent(CreateAccount.this, Menu.class);
-                startActivity(intent);
-                break;
-            }
-        }
-        finish();
+        });
     }
     void Mapping()
     {
+        auth = FirebaseAuth.getInstance();
         et_username = (EditText) findViewById(R.id.et_username);
         et_email = (EditText) findViewById(R.id.et_email);
-        et_phone_number = (EditText) findViewById(R.id.et_phone_number);
         et_password = (EditText) findViewById(R.id.et_password);
         et_confirm_password = (EditText) findViewById(R.id.et_confirm_password);
-        bt_back = (Button) findViewById(R.id.bt_back);
         bt_create = (Button) findViewById(R.id.bt_create);
     }
 }
--- booklend/app/src/main/java/com/example/booklend/Login.java
+++ booklend/app/src/main/java/com/example/booklend/Login.java
@@ -1,16 +1,22 @@
 package com.example.booklend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Login extends AppCompatActivity {
-    private EditText et_username, et_password;
-    private Button bt_sign_in, bt_forget_password, bt_create_account;
+    private FirebaseAuth auth;
+    private EditText et_email, et_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,29 +29,59 @@ public class Login extends AppCompatActivity {
         {
             case R.id.bt_sign_in:
             {
-                Intent intent = new Intent(Login.this, Menu.class);
-                startActivity(intent);
+                String email = et_email.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    if (!password.isEmpty())
+                    {
+                        auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Login.this, Menu.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this, "Login failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                    else {
+                        et_password.setError("Password can't be empty");
+                    }
+                }
+                else if (email.isEmpty())
+                {
+                    et_password.setError("Email can't be empty");
+                }
+                else
+                {
+                    et_password.setError("Invalid email");
+                }
                 break;
             }
             case R.id.bt_create_account: {
                 Intent intent = new Intent(Login.this, CreateAccount.class);
                 startActivity(intent);
+                finish();
                 break;
             }
             case R.id.bt_forget_password: {
                 Intent intent = new Intent(Login.this, ForgetPassword.class);
                 startActivity(intent);
+                finish();
                 break;
             }
         }
-        finish();
     }
     void Mapping()
     {
-        et_username = (EditText) findViewById(R.id.et_username);
+        auth = FirebaseAuth.getInstance();
+        et_email = (EditText) findViewById(R.id.et_email);
         et_password = (EditText)  findViewById(R.id.et_password);
-        bt_sign_in = (Button) findViewById(R.id.bt_sign_in);
-        bt_forget_password = (Button) findViewById(R.id.bt_forget_password);
-        bt_create_account = (Button) findViewById(R.id.bt_create);
     }
 }
